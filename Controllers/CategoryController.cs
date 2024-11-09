c143048a416a1664d6b77f5e3cb2538ba97fe503
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-11-09 16:19:35 +0700

    Thêm giao diện Đổi Mật khẩu

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebGiaoDichViecLam.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryJob()
+        {
+            return View();
+
+        }
+
+
+    }
+}
