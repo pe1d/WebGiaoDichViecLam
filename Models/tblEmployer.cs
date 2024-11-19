66fb0390c62e87478bcdc978ba3e31548e40de35
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-11-19 14:07:05 +0700

    Hoàn thành Đăng ký ứng viên và Đăng ký Nhà tuyển dụng

--- Models/tblEmployer.cs
+++ Models/tblEmployer.cs
@@ -7,10 +7,10 @@ namespace WebGiaoDichViecLam.Models
         [Key]
         public int iEmployerID { get; set; }
 
-        public int FK_iAccountID { get; set; }
+        public int iAccountID { get; set; }
 
 
-        public int FK_iCompanyID { get; set; }
+        public int iCompanyID { get; set; }
 
 
         public string sFullName { get; set; }
@@ -21,8 +21,6 @@ namespace WebGiaoDichViecLam.Models
 
         public string sPhoneNumber { get; set; } = null!;
 
-
-        public virtual tblCompany tblCompany { get; set; } = null!;
 
 
 
