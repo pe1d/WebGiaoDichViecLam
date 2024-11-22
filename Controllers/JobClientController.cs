4007e5d6377ce3895fce9d7eae68e155422f71d8
Xuan Diep <xuandiep003@gmail.com>
2024-11-22 22:46:22 +0700

    Hoàn thành hiện việc làm đã ứng tuyển

--- Controllers/JobClientController.cs
+++ Controllers/JobClientController.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebGiaoDichViecLam.Data;
 using WebGiaoDichViecLam.Models;
+
 //Diep
 namespace WebGiaoDichViecLam.Controllers
 {
@@ -43,6 +44,32 @@ namespace WebGiaoDichViecLam.Controllers
         {
             var job = await _context.tblJob.Include(j => j.TblCategory).Include(j => j.TblCompany).FirstOrDefaultAsync(item => item.iJobID == id);
             ViewBag.job = job;
+            return View();
+        }
+
+        public async Task<IActionResult> MyApplyJob()
+        {
+            var accountId = User.FindFirstValue(ClaimTypes.Sid);
+            if (string.IsNullOrEmpty(accountId))
+            {
+                return View(); // Hoặc xử lý phù hợp nếu người dùng không đăng nhập
+            }
+            var profile = await _context.tblProfileUser
+                .FirstOrDefaultAsync(p => p.iAccountID.ToString() == accountId);
+
+            if (profile == null)
+            {
+                return View(); // Trả về lỗi hoặc redirect nếu không tìm thấy profile
+            }
+
+            ViewBag.jobUserApply = await _context.tblJobUser
+                    .Include(j => j.TblProfileUser) // Include bảng tblProfileUser
+                    .Include(j => j.TblJob)        // Include bảng tblJob
+                        .ThenInclude(job => job.TblCategory) // Include thêm bảng tblCategory từ TblJob
+                    .Include(j => j.TblJob.TblCompany) // Include thêm bảng tblCompany từ TblJob
+                    .Where(j => j.iProfileID == profile.iProfileID)
+                    .ToListAsync();
+
             return View();
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
