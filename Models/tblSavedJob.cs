7c83ed8ac6961684da665da273723459cf1e5f7b
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-11-25 16:31:25 +0700

    Thêm danh mục Việc làm đã lưu Xử lý sự kiện lưu công việc và hủy lưu công việc Thêm View hiện danh sách công việc đã lưu

---
+++ Models/tblSavedJob.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebGiaoDichViecLam.Models
+{
+    public class tblSavedJob
+    {
+        [Key]
+        public int iJobSaveID { get; set; }
+        public int iJobID { get; set; }
+        public int iProfileID { get; set; }
+
+        [ForeignKey("iProfileID")]
+        public virtual tblProfileUser TblProfileUser { get; set; } = null!;
+
+        [ForeignKey("iJobID")]
+        public virtual tblJob TblJob { get; set; } = null!;
+    }
+}
